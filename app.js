32652c74ce74ae82476881091958449a842baff1
beauspot <beauspot@outlook.com>
2023-02-18 09:57:43 +0100

    finished setting up the mongo db connection string to the db on the cloud & setting the start() in the application's entery point.

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require("dotenv").config();
+//async errors
+
+const express = require("express");
+const logger = require("morgan");
+const errHandler = require("./middleware/error-handler");
+const _404NotFound = require("./middleware/not-found");
+const connectDB = require("./db/connect");
+
+const app = express();
+
+// <================= Middleware =================>
+app.use(logger("dev"));
+app.use(express.json());
+app.use(errHandler);
+// <================= Middleware =================>
+
+app.use(_404NotFound);
+
+const PORT = process.env.PORT || 8080;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.info(`App Listening on ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,12 +1,29 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDB = (url) => {
+/* const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-}
+} */
 
-module.exports = connectDB
+// remodifying the above code with async/await synatx
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(
+      url,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      },
+      console.log("Connected to the Mongoose DB")
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = connectDB;
